3181e6ce3db27a3034a77ab7a87a62395927b656
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-12-04 23:02:52 +0100

    Add day 3 part 2

--- src/day_03.rs
+++ src/day_03.rs
@@ -6,6 +6,24 @@ use aoc_runner_derive::{aoc, aoc_generator};
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Rucksack(String, String);
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct ElveGroup(String, String, String);
+
+trait Priority {
+    /// Get the priority of this item for the elves.
+    fn to_prio(self) -> u32;
+}
+
+impl Priority for char {
+    fn to_prio(self) -> u32 {
+        if self >= 'a' && self <= 'z' {
+            self as u32 - 'a' as u32 + 1
+        } else {
+            self as u32 - 'A' as u32 + 27
+        }
+    }
+}
+
 impl Rucksack {
     /// Find all elements which are in both compartements.
     pub fn find_duplicates(&self) -> Vec<char> {
@@ -25,14 +43,67 @@ impl Rucksack {
     }
 }
 
-#[aoc_generator(day3)]
-fn generator_day03(inp: &str) -> Vec<Rucksack> {
+impl ElveGroup {
+    /// Find all elements which are in the rucksacks of all three elves.
+    pub fn find_duplicates(&self) -> Vec<char> {
+        let mut duplicates = HashSet::new();
+
+        let mut chars_set = HashSet::new();
+        for c in self.0.chars() {
+            chars_set.insert(c);
+        }
+
+        for c in self.1.chars() {
+            if chars_set.get(&c).is_some() {
+                duplicates.insert(c);
+            }
+        }
+
+        chars_set = duplicates;
+        duplicates = HashSet::new();
+        for c in self.2.chars() {
+            if chars_set.get(&c).is_some() {
+                duplicates.insert(c);
+            }
+        }
+
+        duplicates.into_iter().collect()
+    }
+}
+
+/// Generator for part 1 of day 3.
+/// It packs the items of all elves into their respective compartements.
+#[aoc_generator(day3, part1)]
+fn generator_day03_part1(inp: &str) -> Vec<Rucksack> {
     let mut rucksacks = vec![];
     for line in inp.lines() {
         let (left, right) = line.split_at(line.len() / 2);
         rucksacks.push(Rucksack(left.to_string(), right.to_string()));
     }
     rucksacks
+}
+
+/// Generator for part 2 of day 3.
+/// It groups three elves as one "ElveGroup".
+#[aoc_generator(day3, part2)]
+fn generator_day03_part2(inp: &str) -> Vec<ElveGroup> {
+    let mut elve_groups = vec![];
+
+    let mut current_elves = vec![];
+
+    for line in inp.lines() {
+        current_elves.push(line);
+        if current_elves.len() == 3 {
+            elve_groups.push(ElveGroup(
+                current_elves[0].to_string(),
+                current_elves[1].to_string(),
+                current_elves[2].to_string(),
+            ));
+            current_elves.clear();
+        }
+    }
+
+    elve_groups
 }
 
 #[aoc(day3, part1)]
@@ -43,13 +114,21 @@ fn day03_part1(rucksacks: &[Rucksack]) -> u32 {
             rucksack
                 .find_duplicates()
                 .iter()
-                .map(|elem| {
-                    if *elem >= 'a' && *elem <= 'z' {
-                        *elem as u32 - 'a' as u32 + 1
-                    } else {
-                        *elem as u32 - 'A' as u32 + 27
-                    }
-                })
+                .map(|elem| elem.to_prio())
+                .sum::<u32>()
+        })
+        .sum()
+}
+
+#[aoc(day3, part2)]
+fn day03_part2(elve_groups: &[ElveGroup]) -> u32 {
+    elve_groups
+        .iter()
+        .map(|elve_group| {
+            elve_group
+                .find_duplicates()
+                .iter()
+                .map(|elem| elem.to_prio())
                 .sum::<u32>()
         })
         .sum()
@@ -67,8 +146,8 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw";
 
     #[test]
-    fn test_generator_day03() {
-        let rucksacks = generator_day03(INPUT);
+    fn test_generator_day03_part1() {
+        let rucksacks = generator_day03_part1(INPUT);
         assert_eq!(
             rucksacks,
             vec![
@@ -86,7 +165,27 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
     }
 
     #[test]
-    fn test_find_duplicates() {
+    fn test_generator_day03_part2() {
+        let elve_groups = generator_day03_part2(INPUT);
+        assert_eq!(
+            elve_groups,
+            vec![
+                ElveGroup(
+                    "vJrwpWtwJgWrhcsFMMfFFhFp".to_string(),
+                    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL".to_string(),
+                    "PmmdzqPrVvPwwTWBwg".to_string()
+                ),
+                ElveGroup(
+                    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn".to_string(),
+                    "ttgJtRGJQctTZtZT".to_string(),
+                    "CrZsJsPPZsGzwwsLwLmpwMDw".to_string()
+                )
+            ]
+        )
+    }
+
+    #[test]
+    fn test_rucksack_find_duplicates() {
         assert_eq!(
             Rucksack("abcde".to_string(), "ABcDe".to_string()).find_duplicates(),
             vec!['c', 'e']
@@ -98,8 +197,30 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
     }
 
     #[test]
+    fn test_elve_group_find_duplicates() {
+        let elve_groups = generator_day03_part2(INPUT);
+        assert_eq!(
+            elve_groups
+                .iter()
+                .map(|group| group
+                    .find_duplicates()
+                    .iter()
+                    .map(|elem| elem.to_prio())
+                    .sum::<u32>())
+                .collect::<Vec<_>>(),
+            vec![18, 52]
+        );
+    }
+
+    #[test]
     fn test_day03_part1() {
-        let rucksacks = generator_day03(INPUT);
+        let rucksacks = generator_day03_part1(INPUT);
         assert_eq!(day03_part1(&rucksacks), 157);
+    }
+
+    #[test]
+    fn test_day03_part2() {
+        let elve_groups = generator_day03_part2(INPUT);
+        assert_eq!(day03_part2(&elve_groups), 70);
     }
 }
