b3c2c12a979fa42c5bda668daa2e0b194f72082d
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-12-05 15:55:08 +0100

    Add day 4 part 1

--- src/day_04.rs
+++ src/day_04.rs
@@ -1,4 +1,4 @@
-use aoc_runner_derive::aoc_generator;
+use aoc_runner_derive::{aoc, aoc_generator};
 
 /// Struct representing a section for elves to clean.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -33,6 +33,17 @@ fn generator_day04(inp: &str) -> Vec<(Section, Section)> {
             (Section::from_string(left), Section::from_string(right))
         })
         .collect()
+}
+
+#[aoc(day4, part1)]
+fn day04_part1(sections: &[(Section, Section)]) -> usize {
+    sections.iter().fold(0, |acc, (left, right)| {
+        acc + if left.contains_section(&right) || right.contains_section(&left) {
+            1
+        } else {
+            0
+        }
+    })
 }
 
 #[cfg(test)]
@@ -82,5 +93,11 @@ mod tests {
     fn test_section_contains_section() {
         assert!(Section(2, 8).contains_section(&Section(3, 5)));
         assert!(!Section(3, 6).contains_section(&Section(3, 7)));
+    }
+
+    #[test]
+    fn test_day04_part1() {
+        let sections = generator_day04(INPUT);
+        assert_eq!(day04_part1(&sections), 2);
     }
 }
